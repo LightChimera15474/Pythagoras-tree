0a353e344f63fc9c22ba18a9fe4654a37abc6b0b
Artem <artemilin347@gmail.com>
2022-07-08 19:51:11 +0300

    Comments added

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ angle = 20
 width = 10
 penSize = 16
 
-# colors param
-defaultColor = "#3d0c02"  # "#000000"
+# colors params
+branchColor = "#3d0c02"
 leafColors = ["#007046", "#20815D", "#00AC6B", "#00CC00", "#008500", "#7B9E00"]
 
 # window settings
 positionX = 0
 positionY = turtle.window_height() / -2
 
+# create the programs
 for i in range(rng):
     tmpStr = ""
     for key in axiom:
         tmpStr += rule.get(key, key)
     axiom = tmpStr
-
 
 # turtle settings
 turtle.hideturtle()
@@ -31,31 +31,31 @@ turtle.setposition(positionX, positionY)
 turtle.left(90)
 turtle.pendown()
 turtle.pensize(penSize)
-turtle.color(defaultColor)
+turtle.color(branchColor)
 turtle.tracer(50)
 
 stack = []
 for command in axiom:
-    if command == "1":
+    if command == "1":  # draw branch
         turtle.forward(width)
-    elif command == '2':
+    elif command == '2':  # draw branch by chance 0.4
         if random() <= 0.4:
             turtle.forward(width)
-    elif command == "0":
+    elif command == "0":  # draw foliage
         turtle.pensize(penSize * 10)
-        color = randint(0, len(leafColors) - 1)
+        color = randint(0, len(leafColors) - 1)  # select the color of leaf
         turtle.color(leafColors[color])
         turtle.forward(width - 5)
-        turtle.color(defaultColor)
-    elif command == "[":
-        penSize *= 0.75
+        turtle.color(branchColor)  # change the color back to branch color
+    elif command == "[":  # save position and direction to steck
+        penSize *= 0.75  # branch thickness reduction
         turtle.pensize(penSize)
         stack.append(penSize)
         stack.append(turtle.xcor())
         stack.append(turtle.ycor())
         stack.append(turtle.heading())
         turtle.left(angle - randint(-8, 8))
-    elif command == "]":
+    elif command == "]":  # load position and direction from steck
         turtle.penup()
         turtle.setheading(stack.pop())
         turtle.sety(stack.pop())
@@ -64,7 +64,6 @@ for command in axiom:
         turtle.pensize(penSize)
         turtle.pendown()
         turtle.right(angle - randint(-8, 8))
-
 
 turtle.update()
 turtle.done()
